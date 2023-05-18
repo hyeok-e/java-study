f7d4fdb1aca5e16dc437dca1a96d399383cae375
Minhyeok <hyeok_e@gooroomee.com>
2023-05-19 02:05:47 +0900

    second-commit(parrot)

--- src/lec03/Scan2.java
+++ src/lec03/Scan2.java
@@ -6,7 +6,7 @@ public class Scan2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Scanner scan2 = new Scanner(System.in);
-
+        Scanner scan3 = new Scanner(System.in);
         // 1.작은 정수가 입력될 때, 2의 32승 까지만 받는다 약 21억
         int a = scan.nextInt();
         System.out.println(a);
@@ -14,6 +14,9 @@ public class Scan2 {
         // 2.큰 정수 2^63 - 1까지 받는다.
         long b = scan2.nextLong();
         System.out.println(b);
+
+        float c = scan3.nextFloat();
+        System.out.println(c);
 
     }
 }
